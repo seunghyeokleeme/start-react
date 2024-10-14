0acbb718465dd2dc53635b4152410dabd25d8733
seunghyeokleeme <seunghyeok.tech@gmail.com>
2024-10-15 07:36:44 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("Call the API..");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Continue" />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
